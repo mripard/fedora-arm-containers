05cd7a7d3ef2153f4b2180b32b5824ed09cb1e76
Maxime Ripard <mripard@kernel.org>
2025-02-17 17:04:41 +0100

    fixup! github: Test that we have a first boot

--- .github/workflows/boot-qemu-image.py
+++ .github/workflows/boot-qemu-image.py
@@ -31,8 +31,9 @@ def main():
     # let the wrapper exec the qemu process.
     time.sleep(1)
 
+
     try:
-        child.expect(["systemd\[\d\]: Detected first boot."], timeout=300)
+        child.expect(["systemd\\[\\d\\]: Detected first boot."], timeout=300)
     except pexpect.EOF as e:
         # Some emulations require a fork of qemu-system, which may be
         # missing on the system, and is not provided by Buildroot.
@@ -52,7 +53,7 @@ def main():
         sys.exit(1)
 
     try:
-        child.expect(["Please enter user name to create (empty to skip):"], timeout=300)
+        child.expect(["Please enter user name to create \\(empty to skip\\):"], timeout=300)
     except pexpect.EOF as e:
         print("Cannot connect to shell.")
         sys.exit(1)
@@ -60,7 +61,7 @@ def main():
         print("System did not boot in time, exiting.")
         sys.exit(1)
 
-    child.sendline("")
+    child.sendline("\r")
 
     try:
         child.expect(["[0-9a-f]{12} login:"], timeout=300)
